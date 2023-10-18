d315872a100e8f1566aac034dd22ddcdaec8f24d
Floria <florian.richet2004@gmail.com>
2023-10-18 16:09:28 +0200

    remarche

--- src/Fonctions/firebaseConfig.tsx
+++ src/Fonctions/firebaseConfig.tsx
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-  };
+  apiKey: 'AIzaSyBLVJ-BSHE-c4bLNoFkm2w-VpELqUQYVEA',
+  authDomain: 'ereaction-32679.firebaseapp.com',
+  projectId: 'ereaction-32679',
+  storageBucket: 'ereaction-32679.appspot.com',
+  messagingSenderId: '1082701041903',
+  appId: '1:1082701041903:web:3912481e52aec1b31f4b1a',
+  measurementId: 'G-82HPE8X2S0',
+};
   
