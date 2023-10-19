9eb825fe9dae859f4c91a0f24216cbf33024ead9
Hywel <bonnefondhywel@gmail.com>
2023-10-19 10:41:06 +0200

    series

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -3,22 +3,19 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import './home.scss';
 
-export const seriesData = [
-  { id: 1, name: 'Avengers', rating: 4.5, imageUrl: 'serie1.jpg', theme: 'superhero' },
-  { id: 2, name: 'James Bond', rating: 3.8, imageUrl: 'serie2.jpg', theme: 'action' },
-  { id: 3, name: 'Sherlock Holmes', rating: 4.2, imageUrl: 'serie3.jpg', theme: 'enquete' },
-  { id: 4, name: 'Ça', rating: 4.8, imageUrl: 'serie4.jpg', theme: 'horreur' },
-  { id: 5, name: 'Mad Max', rating: 2.7, imageUrl: 'serie5.jpg', theme: 'action' },
-];
+interface Movie {
+  id: number;
+  title: string;
+  vote_average: number;
+  poster_path: string;
+}
 
 function Home() {
-  const [series, setSeries] = useState(seriesData);
+  const [series, setSeries] = useState<Movie[]>([]);
   const [sortOrder, setSortOrder] = useState('asc');
   const [sortOrderByName, setSortOrderByName] = useState('asc');
-  const [selectedTheme, setSelectedTheme] = useState('Tous');
 
   useEffect(() => {
-    // Fonction pour récupérer les données des films depuis l'API TMDb
     const fetchSeriesData = async () => {
       try {
         const response = await axios.get(
@@ -42,9 +39,9 @@ function Home() {
     const sortSeries = () => {
       const sortedSeries = [...series];
       if (sortOrder === 'asc') {
-        sortedSeries.sort((a, b) => a.rating - b.rating);
+        sortedSeries.sort((a, b) => a.vote_average - b.vote_average);
       } else {
-        sortedSeries.sort((a, b) => b.rating - a.rating);
+        sortedSeries.sort((a, b) => b.vote_average - a.vote_average);
       }
       setSeries(sortedSeries);
     };
@@ -56,9 +53,9 @@ function Home() {
     const sortedSeriesByName = () => {
       const sortedSeriesByName = [...series];
       if (sortOrderByName === 'asc') {
-        sortedSeriesByName.sort((a, b) => a.name.localeCompare(b.name));
+        sortedSeriesByName.sort((a, b) => a.title.localeCompare(b.title));
       } else {
-        sortedSeriesByName.sort((a, b) => b.name.localeCompare(a.name));
+        sortedSeriesByName.sort((a, b) => b.title.localeCompare(a.title));
       }
       setSeries(sortedSeriesByName);
     };
@@ -66,58 +63,28 @@ function Home() {
     sortedSeriesByName();
   }, [sortOrderByName]);
 
-  const filterSeriesByTheme = (theme: string) => {
-    if (theme === 'Tous') {
-      setSeries(seriesData);
-    } else {
-      const filteredSeries = seriesData.filter((serie) => serie.theme === theme);
-      setSeries(filteredSeries);
-    }
-    setSelectedTheme(theme);
-  };
-
-  const uniqueThemes = [...new Set(seriesData.map((serie) => serie.theme))];
-  uniqueThemes.unshift('Tous');
-
   return (
     <div>
       <h1>LISTE DES FILMS</h1>
-      <button
-        onClick={() => setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc')}
-      >
+      <button onClick={() => setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc')}>
         Trier par note {sortOrder === 'asc' ? 'décroissante' : 'croissante'}
       </button>
-      <button
-        onClick={() => setSortOrderByName(sortOrderByName === 'asc' ? 'desc' : 'asc')}
-      >
+      <button onClick={() => setSortOrderByName(sortOrderByName === 'asc' ? 'desc' : 'asc')}>
         Trier par nom {sortOrderByName === 'asc' ? 'décroissant' : 'croissant'}
       </button>
-      <div>
-        <p>Filtrer par thème :</p>
-        {uniqueThemes.map((theme) => (
-          <button
-            key={theme}
-            onClick={() => filterSeriesByTheme(theme)}
-            className={theme === selectedTheme ? 'active' : ''}
-          >
-            {theme}
-          </button>
-        ))}
-      </div>
       <ul>
         {series.map((serie) => (
           <Link to={`/serie/${serie.id}`} key={serie.id}>
             <li>
-              <img src={serie.imageUrl} alt={serie.name} />
-              <h2>{serie.name}</h2>
-              <p>Note : {serie.rating}</p>
-              <p>Thème : {serie.theme}</p>
+              <img src={`https://image.tmdb.org/t/p/w200/${serie.poster_path}`} alt={serie.title} />
+              <h2>{serie.title}</h2>
+              <p>Note : {serie.vote_average}</p>
             </li>
           </Link>
         ))}
       </ul>
     </div>
   );
-};
+}
 
 export default Home;
--- src/components/serie.tsx
+++ src/components/serie.tsx
@@ -1,24 +1,53 @@
 import { useParams } from 'react-router-dom';
-import { seriesData } from './home';
-import './serie.scss'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import './serie.scss';
+
+interface Movie {
+  id: number;
+  title: string;
+  overview: string;
+  release_date: string;
+  vote_average: number;
+  vote_count: number;
+  poster_path: string;
+}
 
 function Serie() {
   const { id } = useParams();
-  const serie = seriesData.find((item: any) => item.id === parseInt(id || '0', 10));
+  const [serie, setSerie] = useState<Movie | null>(null);
+
+  useEffect(() => {
+    const fetchSerieDetails = async () => {
+      try {
+        const response = await axios.get(`https://api.themoviedb.org/3/movie/${id}`, {
+          params: {
+            api_key: 'c502595c535b3bcebeeec3b468325e4b',
+          },
+        });
+        setSerie(response.data);
+      } catch (error) {
+        console.error('Erreur lors de la récupération des détails du film :', error);
+      }
+    };
+
+    fetchSerieDetails();
+  }, [id]);
 
   if (!serie) {
-    return <div>Série non trouvée.</div>;
+    return <div>Chargement en cours...</div>;
   }
 
   return (
     <div>
-      <h1>Détails de la série : {serie.name}</h1>
-      <img src={serie.imageUrl} alt={serie.name} />
-      <p>Résumé : {serie.name}</p>
-      <p>Distribution : {serie.name}</p>
-      <p>Saisons : {serie.name}</p>
+      <h1>Détails du film : {serie.title}</h1>
+      <img src={`https://image.tmdb.org/t/p/w200/${serie.poster_path}`} alt={serie.title} />
+      <p>Résumé : {serie.overview}</p>
+      <p>Date de sortie : {serie.release_date}</p>
+      <p>Note : {serie.vote_average}</p>
+      <p>Nombre de votes : {serie.vote_count}</p>
     </div>
   );
-};
+}
 
 export default Serie;
